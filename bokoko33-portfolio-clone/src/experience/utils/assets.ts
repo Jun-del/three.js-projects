a902ca1d58021c4a5ec075207d276b888cd712cd
Jun <neohwj28@gmail.com>
2023-06-04 00:37:28 +0800

    Add utilities

---
+++ bokoko33-portfolio-clone/src/experience/utils/assets.ts
@@ -0,0 +1,18 @@
+import { Asset } from "../../types";
+
+type Assets = Asset[];
+
+const assets: Assets = [
+  {
+    name: "room",
+    type: "glb",
+    path: "models/room.glb",
+  },
+  //   {
+  //     name: "monitorScreen",
+  //     type: "videoTexture",
+  //     path: "textures/monitorScreen.mp4",
+  //   },
+];
+
+export default assets;
