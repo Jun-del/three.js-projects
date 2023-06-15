34426b7d723e946241680497963e187a917287bc
Jun <neohwj28@gmail.com>
2023-06-15 23:15:32 +0800

    Add tween

--- bokoko33-portfolio-clone/src/experience/scene/controls.ts
+++ bokoko33-portfolio-clone/src/experience/scene/controls.ts
@@ -6,141 +6,299 @@ import Experience from "../experience";
 import { Themes } from "../../types";
 
 export default class Controls {
-  public experience: Experience;
-  public scene: Experience["scene"];
-  public sizes: Experience["sizes"];
-  public resources: Experience["resources"];
-  public time: Experience["time"];
-  public camera: Experience["camera"];
-  public room: Experience["world"]["room"]["roomScene"];
-  public timeline: GSAPTimeline;
-  public firstMoveTimeline!: GSAPTimeline;
-  public secondMoveTimeline!: GSAPTimeline;
-  public pointLight!: THREE.PointLight;
+	public experience: Experience;
+	public scene: Experience["scene"];
+	public sizes: Experience["sizes"];
+	public resources: Experience["resources"];
+	public time: Experience["time"];
+	public camera: Experience["camera"];
+	public room: Experience["world"]["room"]["roomScene"];
+	public timeline: GSAPTimeline;
+	public firstMoveTimeline!: GSAPTimeline;
+	public secondMoveTimeline!: GSAPTimeline;
+	public thirdMoveTimeline!: GSAPTimeline;
+	public secondPartTimeline!: GSAPTimeline;
+	public pointLight!: THREE.PointLight;
+	public first!: GSAPTween;
+	public second!: GSAPTween;
+	public third!: GSAPTween;
+	public fourth!: GSAPTween;
+	public fifth!: GSAPTween;
+	public sixth!: GSAPTween;
 
-  constructor() {
-    this.experience = new Experience();
-    this.scene = this.experience.scene;
-    this.sizes = this.experience.sizes;
-    this.resources = this.experience.resources;
-    this.time = this.experience.time;
-    this.camera = this.experience.camera;
-    this.room = this.experience.world.room.roomScene;
+	constructor() {
+		this.experience = new Experience();
+		this.scene = this.experience.scene;
+		this.sizes = this.experience.sizes;
+		this.resources = this.experience.resources;
+		this.time = this.experience.time;
+		this.camera = this.experience.camera;
+		this.room = this.experience.world.room.roomScene;
 
-    this.room.children.forEach((child) => {
-      if (child.type === "PointLight") {
-        this.pointLight = child as THREE.PointLight;
-      }
-    });
-    this.timeline = GSAP.timeline();
+		this.room.children.forEach((child) => {
+			if (child.type === "PointLight") {
+				this.pointLight = child as THREE.PointLight;
+			}
+		});
+		this.timeline = GSAP.timeline();
 
-    // Add markers property to ScrollTrigger defaults
-    // ScrollTrigger.defaults({
-    //   markers: true,
-    // });
+		// Add markers property to ScrollTrigger defaults
+		// ScrollTrigger.defaults({
+		//   markers: true,
+		// });
 
-    GSAP.registerPlugin(ScrollTrigger);
+		GSAP.registerPlugin(ScrollTrigger);
 
-    this.setScrollTrigger();
-  }
+		this.setScrollTrigger();
+	}
 
-  setScrollTrigger() {
-    ScrollTrigger.matchMedia({
-      // large: Desktop and up
-      "(min-width: 969px)": () => {
-        // * First Section
-        this.firstMoveTimeline = GSAP.timeline({
-          scrollTrigger: {
-            trigger: ".first-move",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: 0.6,
-            invalidateOnRefresh: true,
-          },
-        });
-        this.firstMoveTimeline.to(this.room.position, {
-          x: () => {
-            return this.sizes.width * 0.0014;
-          },
-        });
+	setScrollTrigger() {
+		ScrollTrigger.matchMedia({
+			// large: Desktop and up
+			"(min-width: 969px)": () => {
+				// * First Section
+				this.room.scale.set(0.1, 0.1, 0.1);
 
-        // * Second Section
-        this.secondMoveTimeline = GSAP.timeline({
-          scrollTrigger: {
-            trigger: ".second-move",
-            start: "top top",
-            end: "bottom bottom",
-            scrub: 0.6,
-            invalidateOnRefresh: true,
-          },
-        });
-        this.secondMoveTimeline
-          .to(
-            this.room.position,
-            {
-              x: () => {
-                return 1;
-              },
-              z: () => {
-                return this.sizes.height * 0.0032;
-              },
-            },
-            "same"
-          )
-          .to(
-            this.room.scale,
-            {
-              x: 0.4,
-              y: 0.4,
-              z: 0.4,
-            },
-            "same"
-          );
-      },
+				this.firstMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".first-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				});
+				this.firstMoveTimeline.to(this.room.position, {
+					x: () => {
+						return this.sizes.width * 0.0014;
+					},
+				});
 
-      // medium: Mobile
-      "(max-width: 968px)": () => {},
+				// * Second Section
+				this.secondMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".second-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				});
+				this.secondMoveTimeline
+					.to(
+						this.room.position,
+						{
+							x: () => {
+								return -3.5;
+							},
+							z: () => {
+								// return this.sizes.height * 0.0032;
+								return this.sizes.height * 0.005;
+							},
+						},
+						"same"
+					)
+					.to(
+						this.room.scale,
+						{
+							x: 0.4,
+							y: 0.4,
+							z: 0.4,
+						},
+						"same"
+					);
 
-      all: function () {},
-    });
+				// * Third Section (Camera)
+				this.thirdMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".third-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				});
+				this.thirdMoveTimeline.to(this.camera.orthographicCamera.position, {
+					x: -8,
+					y: -1.5,
+				});
+			},
 
-    // this.timeline.to(this.room.position, {
-    //   x: () => {
-    //     return this.sizes.width * 0.0012;
-    //   },
-    //   scrollTrigger: {
-    //     trigger: ".first-move",
-    //     // marker: true,
-    //     start: "top top",
-    //     end: "bottom bottom",
-    //     scrub: 0.6,
-    //     invalidateOnRefresh: true,
-    //   },
-    // });
-  }
+			// medium: Mobile
+			"(max-width: 968px)": () => {
+				// Resets
+				this.room.scale.set(0.07, 0.07, 0.07);
+				this.room.position.set(0, 0, 0);
 
-  setRoomLight(theme: Themes) {
-    ScrollTrigger.matchMedia({
-      "(min-width: 969px)": () => {
-        if (theme === "dark") {
-          this.secondMoveTimeline.to(
-            this.pointLight,
-            {
-              intensity: 0.05 * 4,
-              distance: 0.5 * 4,
-            },
-            "same"
-          );
-        }
-      },
+				// * First Section
+				this.firstMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".first-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				}).to(this.room.scale, {
+					x: 0.1,
+					y: 0.1,
+					z: 0.1,
+				});
 
-      "(max-width: 968px)": () => {},
+				// * Second Section
+				this.secondMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".second-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				})
+					.to(
+						this.room.scale,
+						{
+							x: 0.25,
+							y: 0.25,
+							z: 0.25,
+						},
+						"same"
+					)
+					.to(
+						this.room.position,
+						{
+							x: -0.5,
+							z: 4,
+						},
+						"same"
+					);
 
-      all: function () {},
-    });
-  }
+				// * Third Section
+				this.thirdMoveTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".third-move",
+						start: "top top",
+						end: "bottom bottom",
+						scrub: 0.6,
+						invalidateOnRefresh: true,
+					},
+				}).to(this.room.position, {
+					x: 1.3,
+					z: -4.5,
+				});
+			},
 
-  resize() {}
+			all: () => {
+				// * Platform animations
+				this.secondPartTimeline = GSAP.timeline({
+					scrollTrigger: {
+						trigger: ".third-move",
+						start: "top top",
+					},
+				});
 
-  update() {}
+				this.room.children.forEach((child) => {
+					if (child.name === "MiniFloor") {
+						console.log(child.position);
+
+						// !! Original x: -5.846341133117676, y: -0.5232429504394531, z: 11.979839324951172
+
+						this.first = GSAP.to(child.position, {
+							x: -5.846341133117676,
+							z: 11.979839324951172,
+							duration: 0.3,
+						});
+					}
+
+					if (child.name === "MailBox") {
+						this.second = GSAP.to(child.scale, {
+							x: 1,
+							y: 1,
+							z: 1,
+							ease: "back.out(2)",
+							duration: 0.3,
+						});
+					}
+
+					if (child.name === "Lamp") {
+						this.third = GSAP.to(child.scale, {
+							x: 1,
+							y: 1,
+							z: 1,
+							ease: "back.out(2)",
+							duration: 0.3,
+						});
+					}
+
+					if (child.name === "FloorFirst") {
+						this.fourth = GSAP.to(child.scale, {
+							x: 1,
+							y: 1,
+							z: 1,
+							ease: "back.out(2)",
+							duration: 0.3,
+						});
+					}
+
+					if (child.name === "FloorSecond") {
+						this.fifth = GSAP.to(child.scale, {
+							x: 1,
+							y: 1,
+							z: 1,
+							ease: "back.out(2)",
+							duration: 0.3,
+						});
+					}
+
+					if (child.name === "FloorThird") {
+						this.sixth = GSAP.to(child.scale, {
+							x: 1,
+							y: 1,
+							z: 1,
+							ease: "back.out(2)",
+							duration: 0.3,
+						});
+					}
+				});
+				this.secondPartTimeline.add(this.first);
+				this.secondPartTimeline.add(this.second);
+				this.secondPartTimeline.add(this.third, "-=0.05");
+				this.secondPartTimeline.add(this.fourth, "-=0.2");
+				this.secondPartTimeline.add(this.fifth, "-=0.2");
+				this.secondPartTimeline.add(this.sixth, "-=0.2");
+			},
+		});
+	}
+
+	setRoomLight(theme: Themes) {
+		ScrollTrigger.matchMedia({
+			"(max-width: 969px)": () => {
+				// if (theme === "dark") {
+				// this.firstMoveTimeline.to(
+				// 	this.pointLight,
+				// 	{
+				// 		distance: 1,
+				// 	},
+				// 	"same"
+				// );
+				// }
+			},
+			"(max-width: 968px)": () => {
+				// if (theme === "dark") {
+				// this.secondMoveTimeline.to(
+				// 	this.pointLight,
+				// 	{
+				// 		distance: 1,
+				// 	},
+				// 	"same"
+				// );
+				// }
+			},
+			all: function () {},
+		});
+	}
+
+	resize() {}
+
+	update() {}
 }
--- bokoko33-portfolio-clone/src/experience/scene/room.ts
+++ bokoko33-portfolio-clone/src/experience/scene/room.ts
@@ -5,110 +5,125 @@ import Experience from "../experience";
 import { AssetItem, Themes } from "../../types";
 
 export default class Room {
-  public experience: Experience;
-  public scene: Experience["scene"];
-  public resources: Experience["resources"];
-  public room: AssetItem;
-  public roomScene: THREE.Object3D;
-  public lerp: {
-    current: number;
-    target: number;
-    ease: number;
-  };
-  public rotation!: number;
-  public pointLight!: THREE.PointLight;
-  public sphereSize!: number;
-  public pointLightHelper!: THREE.PointLightHelper;
+	public experience: Experience;
+	public scene: Experience["scene"];
+	public resources: Experience["resources"];
+	public room: AssetItem;
+	public roomScene: THREE.Object3D;
+	public lerp: {
+		current: number;
+		target: number;
+		ease: number;
+	};
+	public rotation!: number;
+	public pointLight!: THREE.PointLight;
+	public sphereSize!: number;
+	public pointLightHelper!: THREE.PointLightHelper;
 
-  constructor() {
-    this.experience = new Experience();
-    this.scene = this.experience.scene;
-    this.resources = this.experience.resources;
-    this.room = this.resources.items.room;
-    this.roomScene = this.room.scene as THREE.Object3D;
+	constructor() {
+		this.experience = new Experience();
+		this.scene = this.experience.scene;
+		this.resources = this.experience.resources;
+		this.room = this.resources.items.room;
+		this.roomScene = this.room.scene as THREE.Object3D;
 
-    this.lerp = {
-      current: 0,
-      target: 0,
-      ease: 0.1,
-    };
+		this.lerp = {
+			current: 0,
+			target: 0,
+			ease: 0.1,
+		};
 
-    this.setModel();
-    this.onMouseMove();
-  }
+		this.setModel();
+		this.onMouseMove();
+	}
 
-  setModel() {
-    this.roomScene.children.forEach((child) => {
-      child.castShadow = true;
-      child.receiveShadow = true;
+	setModel() {
+		this.roomScene.children.forEach((child) => {
+			child.castShadow = true;
+			child.receiveShadow = true;
 
-      if (child instanceof THREE.Group) {
-        child.children.forEach((groupChild) => {
-          groupChild.castShadow = true;
-          groupChild.receiveShadow = true;
-        });
-      }
+			if (child instanceof THREE.Group) {
+				child.children.forEach((groupChild) => {
+					groupChild.castShadow = true;
+					groupChild.receiveShadow = true;
+				});
+			}
 
-      if (child.name === "TableItems") {
-        const meshChild = child.children[2] as THREE.Mesh;
-        meshChild.material = new THREE.MeshPhysicalMaterial({
-          color: 0xc2cee6,
-          roughness: 0,
-          ior: 3,
-          transmission: 1,
-          opacity: 1,
-        });
-      }
+			if (child.name === "TableItems") {
+				const meshChild = child.children[2] as THREE.Mesh;
+				meshChild.material = new THREE.MeshPhysicalMaterial({
+					color: 0xc2cee6,
+					roughness: 0,
+					ior: 3,
+					transmission: 1,
+					opacity: 1,
+				});
+			}
 
-      if (child.name === "Monitor") {
-        (child.children[1] as THREE.Mesh).material =
-          new THREE.MeshBasicMaterial({
-            map: this.resources.items.monitorScreen as THREE.Texture,
-          });
-      }
+			if (child.name === "Monitor") {
+				(child.children[1] as THREE.Mesh).material =
+					new THREE.MeshBasicMaterial({
+						map: this.resources.items.monitorScreen as THREE.Texture,
+					});
+			}
 
-      if (child.name === "TableItems") {
-        (child.children[16] as THREE.Mesh).material =
-          new THREE.MeshBasicMaterial({
-            map: this.resources.items.laptopScreen as THREE.Texture,
-          });
-      }
-    });
+			if (child.name === "TableItems") {
+				(child.children[16] as THREE.Mesh).material =
+					new THREE.MeshBasicMaterial({
+						map: this.resources.items.laptopScreen as THREE.Texture,
+					});
+			}
 
-    this.pointLight = new THREE.PointLight(0xffffff, 0, 0.5);
-    this.pointLight.position.set(0.2, 10, -12);
-    this.roomScene.add(this.pointLight);
+			if (child.name === "MiniFloor") {
+				child.position.x = -1.87767;
+				child.position.z = 8.33244;
+			}
 
-    this.scene.add(this.roomScene);
-    this.roomScene.scale.set(0.1, 0.1, 0.1);
-  }
+			if (
+				child.name === "MailBox" ||
+				child.name === "Lamp" ||
+				child.name === "FloorFirst" ||
+				child.name === "FloorSecond" ||
+				child.name === "FloorThird"
+			) {
+				child.scale.set(0, 0, 0);
+			}
+		});
 
-  setModelLight(theme: Themes) {
-    if (theme === "dark") {
-      this.pointLight.intensity = 0.05;
-    } else {
-      this.pointLight.intensity = 0;
-    }
-  }
+		this.pointLight = new THREE.PointLight(0xffffff, 0, 0.5);
+		this.pointLight.position.set(0.2, 10, -12.5);
+		this.roomScene.add(this.pointLight);
 
-  onMouseMove() {
-    window.addEventListener("mousemove", (event) => {
-      this.rotation =
-        ((event.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
+		this.scene.add(this.roomScene);
+		this.roomScene.scale.set(0.1, 0.1, 0.1);
+	}
 
-      this.lerp.target = this.rotation * 0.08;
-    });
-  }
+	setModelLight(theme: Themes) {
+		if (theme === "dark") {
+			this.pointLight.intensity = 0.05;
+		} else {
+			this.pointLight.intensity = 0;
+		}
+	}
 
-  resize() {}
+	onMouseMove() {
+		window.addEventListener("mousemove", (event) => {
+			this.rotation =
+				((event.clientX - window.innerWidth / 2) * 2) / window.innerWidth;
 
-  update() {
-    this.lerp.current = GSAP.utils.interpolate(
-      this.lerp.current,
-      this.lerp.target,
-      this.lerp.ease
-    );
+			this.lerp.target = this.rotation * 0.08;
+		});
+	}
 
-    this.roomScene.rotation.y = this.lerp.current;
-  }
+	resize() {}
+
+	update() {
+		this.lerp.current = GSAP.utils.interpolate(
+			this.lerp.current,
+			this.lerp.target,
+			this.lerp.ease
+		);
+
+		this.roomScene.rotation.y = this.lerp.current;
+	}
 }
